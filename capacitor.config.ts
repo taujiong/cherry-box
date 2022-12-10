79fb16b402db18ef984d9c4474b2855e1309b2c7
taujiong <taujiong@outlook.com>
2022-12-10 20:39:02 +0800

    chore: create app from ionic starter

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cherry-box',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
