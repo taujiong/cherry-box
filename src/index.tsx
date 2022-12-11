b487e68ad50c3d9629cb5d8b7ec106724f7f2150
taujiong <taujiong@outlook.com>
2022-12-11 12:55:20 +0800

    refactor: postcss-import to gather all styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,3 @@
-import '@ionic/react/css/core.css'
-import '@ionic/react/css/normalize.css'
-import '@ionic/react/css/structure.css'
-import '@ionic/react/css/typography.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
